6f1cb9e97c5230fb2efa432754f68a037b3af2f1
hex <irischen0505@gmail.com>
2023-06-02 15:56:20 -0700

    sdfasd

--- script.js
+++
@@ -1,7 +0,0 @@
-const notionDocId = "fbb70bb7f9024b72ba810205f020aa3d"
-
-fetch("https://potion-api.now.sh/html?id=" + notionDocId)
-  .then(res => res.text())
-  .then(text => {
-    document.querySelector("main").innerHTML = text
-  })
